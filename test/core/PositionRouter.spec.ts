43ae4ac911cd0bd3626a7c8ce19f70c016a26285
0xshoegazer <0xshoegazer@gmail.com>
2024-01-04 16:36:16 -0500

    spec fixture smoke test

--- test/core/PositionRouter.spec.ts
+++ test/core/PositionRouter.spec.ts
@@ -1,7 +1,10 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
+import { commonContractsFixture } from '../fixtures/base.fixture';
 
 describe('PositionRouter', () => {
   it('works', async () => {
+    const {} = await loadFixture(commonContractsFixture);
     expect(true).to.eq(true);
   });
 });
---
+++ test/fixtures/base.fixture.ts
@@ -0,0 +1,5 @@
+import { ethers } from 'hardhat';
+
+export async function commonContractsFixture() {
+  return {};
+}
